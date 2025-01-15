030bf5490481826f56ea6ed04df924fac50a333d
MarkuwM <silvaalderlindson@gmail.com>
2025-01-15 19:14:21 -0300

    é hoje

--- src/Seminario/faculdade/Felype.java
+++ src/Seminario/faculdade/Felype.java
@@ -1,16 +1,8 @@
 package Seminario.faculdade;
 
-import java.util.Optional;
-
 public class Felype {
     public static void main(String[] args) {
         String nome = null;
         System.out.println(nome.toUpperCase());
-
-       /* if (nome.isPresent()){
-            System.out.println("Valor existe");
-        }else {
-            System.out.println("Valor nulo");
-        }*/
     }
 }
--- src/Seminario/faculdade/MetodosOptional.java
+++ src/Seminario/faculdade/MetodosOptional.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 public class MetodosOptional {
     public static void main(String[] args) {
         Optional<String> nome = buscarNome();
+
         //1
         nome.ifPresent(valor -> System.out.println("Nome encontrado: " + valor));
         //2
